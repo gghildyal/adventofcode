b1e611f97d1ea38bb051c56c23888b1fa205265f
Gaurav <gaurav.ghildyal@itoworld.com>
2021-12-06 19:44:17 +0000

    Day 06: Part two. Add an optimised implementation using maps rather than lists.

--- 2021/06/lanternfish.ex
+++ 2021/06/lanternfish.ex
@@ -32,3 +32,50 @@ defmodule Lanternfish do
   end
 
 end
+
+defmodule LanternfishOptimized do
+  @filename "./input.txt"
+  @input [3, 4, 3, 1, 2]
+  @num_days 256
+
+  def get_total(fishes, num) when num == @num_days do
+    fishes
+  end
+
+  def get_total(fishes, num) do
+    fishes
+    |> Enum.reduce(fishes, fn {life, freq}, acc ->#
+        cond do
+          life == 0 ->
+            acc = Map.put(acc, 6, freq)
+            Map.put(acc, 8, freq)
+          life == 7 ->
+            Map.update(acc, life - 1, freq, &(&1 + freq))
+          true ->
+            Map.put(acc, life - 1, freq)
+        end
+      end)
+    |> get_total(num + 1)
+  end
+
+  def read() do
+    File.stream!(@filename)
+    |> Stream.map(&String.trim/1)
+    |> Enum.flat_map(&String.split(&1, ","))
+    |> Enum.map(&String.to_integer/1)
+  end
+
+  def get_map(fishes) do
+    Map.new(Enum.zip(Enum.to_list(0..9), List.duplicate(0, 9)))
+    |> Map.merge(Enum.frequencies(fishes))
+  end
+
+  def process() do
+    read()
+    |> get_map()
+    |> get_total(0)
+    |> Map.values
+    |> Enum.sum
+  end
+
+end
