1ae69f8fe055010f05bf2d4a869fedc31ff91b05
Gaurav <gaurav.ghildyal@itoworld.com>
2021-12-07 06:36:05 +0000

    Day07: Part one.

---
+++ 2021/07/crabs.ex
@@ -0,0 +1,26 @@
+defmodule AlignCrabs do
+  @input [16,1,2,0,4,2,7,1,2,14]
+  @filename "./input.txt"
+
+  def align_cost(move_to, positions) do
+    positions
+      |> Enum.map(fn pos ->
+          abs(pos - move_to)
+        end)
+      |> Enum.sum()
+  end
+
+  def read() do
+    File.stream!(@filename)
+    |> Stream.map(&String.trim/1)
+    |> Enum.flat_map(&String.split(&1, ","))
+    |> Enum.map(&String.to_integer/1)
+  end
+
+  def process do
+    inputs = read()
+    Enum.to_list(Enum.min(inputs)..Enum.max(inputs))
+    |> Enum.map(&(align_cost(&1, inputs)))
+    |> Enum.min()
+  end
+end
