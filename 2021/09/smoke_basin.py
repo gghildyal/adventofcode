17037a5843a22332bbe8aee6f391c9db654307d7
Gaurav <gaurav.ghildyal@itoworld.com>
2021-12-10 22:22:00 +0000

    Day09: Part 2.

--- 2021/09/smoke_basin.py
+++ 2021/09/smoke_basin.py
@@ -1,10 +1,13 @@
+from functools import reduce
+
 class SmokeBasin:
     input_file = "./input.txt"
 
     def __init__(self):
         self.matrix = [[]]
         self.read()
-        self.low_points = []
+        self.low_points = {}
+        self.basin = {}
 
     def read(self):
         with open(self.input_file) as f:
@@ -41,9 +44,33 @@ class SmokeBasin:
                 entry = self.matrix[row_num][column_num]
                 adjacent = self.get_adjacent(row_num, column_num)
                 if entry < min(adjacent):
-                    self.low_points.append(entry)
-        print(sum([x + 1 for x in self.low_points]))
+                    self.low_points[(row_num, column_num)] = entry
+        print(sum([x + 1 for x in self.low_points.values()]))
+    
+    def traverse(self, row, col, visited):
+        if row < 0 or row > len(self.matrix) - 1:
+            return visited
+        if col < 0 or col > len(self.matrix[0]) - 1:
+            return visited
+        if (row, col) in visited:
+            return visited
+        if self.matrix[row][col] == 9:
+            return visited
+       
+        visited[(row, col)] = self.matrix[row][col]
+        self.traverse(row+1, col, visited)
+        self.traverse(row-1, col, visited)
+        self.traverse(row, col+1, visited)
+        self.traverse(row, col-1, visited)
+        return visited
+    
+    def process_2(self):
+        for (row, column) in self.low_points:
+            self.basin[(row, column)] = len(self.traverse(row, column, {}))
+        print(reduce((lambda x, y: x * y), sorted(self.basin.values(), reverse=True)[:3]))
+
 
 if __name__ == '__main__':
     sb = SmokeBasin()
     sb.process_1()
+    sb.process_2()
